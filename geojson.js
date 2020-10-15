ddaa1936f724a26e87369fed4fd7e1d37d6464bb
abbyjackowski <72033757+abbyjackowski@users.noreply.github.com>
2020-10-15 11:11:55 -0500

    Update Geojson

--- geojson.js
+++ geojson.js
@@ -6,174 +6,200 @@
 //
 //    See example below:
 
+
 var json = {
   "type": "FeatureCollection",
   "features": [
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#555555",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#555555",
-        "fill-opacity": 0.5,
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg",
-        "title": "Nebraska State Capitol",
-        "description": "Some description about something that happened at the capitol"
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#ee05ff",
+        "marker-color": "#2a9192",
         "marker-size": "medium",
         "marker-symbol": "",
-        "ingredients": "rhubarb, strawberries, egg, milk, butter, so much sugar",
-        "thanks": "thanks to Linnea for giving me a pie",
-        "title": "Starting Point",
-        "image": "https://s3.amazonaws.com/omeka-net/53984/archive/square_thumbnails/e5847940c8baa5e9e50ad71ee468ba0e.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1600905600&Signature=PapYafLVt%2BGfMiI%2F06Ibpay%2FXPk%3D"
+        "Title": "Sign outside of Roots Collective",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/aaa255e574bdb2aa2c0610e2afa788ef.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603324800&Signature=xwBypvtQ7dxLDpS4J5s0zBcS2cg%3D",
+        "description": "A hair salon stating that a mask is a required to enter the business.",
+        "date": "10/9/2020"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70299053192137,
-          40.81015513625622
+          -96.70584842562675,
+          40.81066063129666
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#ff5100",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "title": "Path to the library",
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg"
+        "marker-color": "#400fb3",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Lack of mask requirement sign at Better Life Nutrition",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/9f2ae52658cee87a5ddfdd00daa07726.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603324800&Signature=KzLm42jGWJtpnQeKW7bpVP9BwxE%3D",
+        "description": "A business that requires no masks and no social distancing",
+        "date": "10/9/2020"
       },
       "geometry": {
-        "type": "LineString",
+        "type": "Point",
         "coordinates": [
-          [
-            -96.69979333877563,
-            40.80922127986828
-          ],
-          [
-            -96.69986844062804,
-            40.811389430620544
-          ],
-          [
-            -96.6997504234314,
-            40.811405671182825
-          ],
-          [
-            -96.6996967792511,
-            40.81468618325975
-          ],
-          [
-            -96.69984698295592,
-            40.8146943031379
-          ],
-          [
-            -96.69984698295592,
-            40.816837916207085
-          ],
-          [
-            -96.70262575149535,
-            40.81687851427114
-          ]
+          -96.70584306120872,
+          40.81050431396455
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#d4ff00",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#0060ff",
-        "fill-opacity": 0.2,
-        "title": "Love Library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/189753833@N07/50237618773/in/album-72157715528007683/\" title=\"yawn_connie\"><img src=\"https://live.staticflickr.com/65535/50237618773_c9052299b5_w.jpg\" width=\"300\" height=\"400\" alt=\"yawn_connie\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "random": "Some other random field"
+        "marker-color": "#cb1090",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Hopcat practicing social distancing guidelines",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/96e981f2b5e1d8df0cedad10d65074ea.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603324800&Signature=TZjSkveF1tMdUlFhoN7km4hj250%3D",
+        "description": "Hopcat has their tables at a safe distancing for it’s customers and you can see masks on employees and some customers.",
+        "date": "10/9/2020"
       },
       "geometry": {
-        "type": "Polygon",
+        "type": "Point",
         "coordinates": [
-          [
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ],
-            [
-              -96.70216441154479,
-              40.81700030831414
-            ],
-            [
-              -96.70187473297119,
-              40.817235776163265
-            ],
-            [
-              -96.70193910598755,
-              40.81753619944697
-            ],
-            [
-              -96.70217514038086,
-              40.81769858984467
-            ],
-            [
-              -96.70218586921692,
-              40.818104564099706
-            ],
-            [
-              -96.70297980308533,
-              40.81808832517719
-            ],
-            [
-              -96.70300126075745,
-              40.817609275175144
-            ],
-            [
-              -96.70323729515076,
-              40.81742252593206
-            ],
-            [
-              -96.70299053192137,
-              40.81704902586876
-            ],
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ]
-          ]
+          -96.71203359961508,
+          40.814398941930676
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#2cb579",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "The empty are in the Haymarket",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/68bff762d96ad10e63da9027df7a208c.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603324800&Signature=jIbIel%2BX09L8Ss7n9WLP4%2F%2Fsqf8%3D",
+        "description": "The Haymarket is usually packed with people but here you can see that there is no one out.",
+        "date": "10/9/2020"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.71125710010529,
+          40.81567680105924
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#e29898",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Crowded groups of people at Oso Burrito.",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/63b37a7e8737e398274a838390284871.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603324800&Signature=FEcJkbxOHi6q3%2BgeY4J7Svc0x1E%3D",
+        "description": "At Oso burrito there were close people packed into the outdoor area.",
+        "date": "10/9/2020"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70000925660132,
+          40.81344992021697
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#f7c0d6",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Several empty parking spots near Lazlo’s",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/4b6d983b3c34c99ba2cb3c88c66a818c.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603929600&Signature=mW7hwN%2BLoUKXr37n1oS0Wh2IpmE%3D",
+        "description": "There were several parking spots open near the restaurants located in the Haymarket.",
+        "date": "10/9/2020"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.71099156141281,
+          40.815418998260675
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#c0b1f7",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Social distancing at the Railyard",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/3942125576b073d8f4fe7857485de49b.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603929600&Signature=uJkLDVC8f5YsGEQb%2BKuC%2Bnbw3os%3D",
+        "description": "The Railyard has placed all of its table at least six feet from each to maintain a safe environment for its customers.",
+        "date": "10/9/2020"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.71160846948624,
+          40.81649689148833
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#91d5f2",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Individuals wearing masks as they walk downtown",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/3b641767cfeb0e8a99c234a369579cb5.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603929600&Signature=XU5KGy7u1Bb4YHKcOptYWOmRsUI%3D",
+        "description": "Most of the people seen downtown are wearing masks when walking from location to location.",
+        "date": "10/9/2020"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70288056135178,
+          40.813490520354335
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#f2563a",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "An empty outdoor section at Lazlo’s",
+        "image": "https://dhpub2.omeka.net/items/show/9",
+        "description": "Lazlo’s was seen to have much less customers in the outdoor area than I have seen before Covid.",
+        "date": "10/9/2020"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.71075016260147,
+          40.814914046963075
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#80ffc1",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Several guests enjoying a meal at The Oven",
+        "image": "https://s3.amazonaws.com/omeka-net/55245/archive/square_thumbnails/c676202e5d93e59a7c141f708a60af57.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1603929600&Signature=BS1u3iTF8Ls3Hg5RSl05fxNLR3s%3D",
+        "description": "The Oven had several customs sitting fairly closely in the outdoor are.",
+        "date": "10/9/2020"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.71016678214073,
+          40.814986618089556
         ]
       }
     }
   ]
 }
+
